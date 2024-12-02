1fcb96ed713d96a130718166413a0560f8855072
Scripter36 <1350adwx@gmail.com>
2024-12-02 17:00:38 +0900

    Upgrade to pytorch 1.13.1 / cuda 11.7

---
+++ installation.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Create conda environment
+conda env create -f diffrl_conda_upgrade.yml
+
+# Activate conda environment
+source activate shac
+
+# Install the package
+cd dflex
+pip install -e .
+cd ../externals/rl_games
+pip install -e .
+
+# Link cuda
+ln -s $CONDA_PREFIX/lib $CONDA_PREFIX/lib64
+
+# go to examples
+cd ../../examples
