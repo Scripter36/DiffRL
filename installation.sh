59cbb168406d656ea118930b7f5366d9165de05c
Scripter36 <52183090+hamasoon@users.noreply.github.com>
2024-12-24 13:32:50 +0900

    remove legacy environment, integrate rl_games installation to conda env, add running script

--- installation.sh
+++ installation.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
 
 # Create conda environment
-conda env create -f diffrl_conda_upgrade.yml
-
-# Activate conda environment
-source activate diffrl_warp
-
-# Install the package
-cd externals/rl_games
-pip install -e .
-
-# go to examples
-cd ../../examples
+conda env create -f diffrl_conda.yml
