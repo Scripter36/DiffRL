gradient not exploding, but incorrect physics on visualization

algorithms/shac.py
@@ -9,7 +9,6 @@ from multiprocessing.sharedctypes import Value
 import sys, os
 
 from torch.nn.utils.clip_grad import clip_grad_norm_
-from torchviz import make_dot
 
 project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(project_dir)
@@ -407,7 +406,7 @@ class SHAC:
             self.time_report.start_timer("forward simulation")
             actor_loss = self.compute_actor_loss()
             self.time_report.end_timer("forward simulation")
-            # make_dot(actor_loss).render("actor_loss", format="png")
+
             self.time_report.start_timer("backward simulation")
             actor_loss.backward()
             self.time_report.end_timer("backward simulation")