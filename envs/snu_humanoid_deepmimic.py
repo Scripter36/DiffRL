fix: correct dt on joint_qd calculation

envs/snu_humanoid_deepmimic.py
@@ -164,7 +164,7 @@ class SNUHumanoidDeepMimicEnv(DFlexEnv):
 
         # load reference motion
         self.reference_frame_time, self.reference_frame_count, self.reference_joint_q, self.reference_joint_q_mask, self.reference_joint_qd = \
-            lu.load_bvh(os.path.join(self.asset_folder, "motion/walk.bvh"), self.skeletons[0].bvh_map, self.model, self.dt)
+            lu.load_bvh(os.path.join(self.asset_folder, "motion/walk.bvh"), self.skeletons[0].bvh_map, self.model)
 
         # end effector indices
         self.end_effector_indices = [4, 9] # FootThumbR, FootThumbL
@@ -461,8 +461,8 @@ class SNUHumanoidDeepMimicEnv(DFlexEnv):
             if self.stochastic_init:
                 self.progress_buf[env_ids] = 0
                 # randomize the reset start frame to learn all reference frames uniformly
-                # self.offset_buf[env_ids] = torch.floor(torch.rand(len(env_ids), device=self.device) * (self.reference_frame_count * self.reference_frame_time / self.dt - 1)).long()
-                self.offset_buf[env_ids] = 0
+                self.offset_buf[env_ids] = torch.floor(torch.rand(len(env_ids), device=self.device) * (self.reference_frame_count * self.reference_frame_time / self.dt - 1)).long()
+                # self.offset_buf[env_ids] = 0
                 self.start_frame_offset = 0
                 self.reference_frame[env_ids] = 0
                 self.reference_pos_offset[env_ids] = self.start_reference_pos_offset[env_ids].clone()

utils/load_utils.py
@@ -795,7 +795,6 @@ def load_bvh(
     filename,
     bvh_link_map: "dict[str, int]",
     model: df.Model,
-    dt: float,
     pos_scale: float = 0.01,
 ) -> "tuple[float, int, torch.Tensor, torch.Tensor]":
     # 1. load skeleton structure, and map the bvh joint names to indices.
@@ -944,13 +943,13 @@ def load_bvh(
                 w = tu.angular_velocity(
                     joint_q[frame_indices, start + 3 : start + 7],
                     joint_q[frame_indices + 1, start + 3 : start + 7],
-                    dt
+                    frame_time
                 )
                 # calculate v
                 v = (
                     joint_q[frame_indices + 1, start : start + 3]
                     - joint_q[frame_indices, start : start + 3]
-                ) / dt - torch.cross(
+                ) / frame_time - torch.cross(
                     w,
                     joint_q[frame_indices, start : start + 3],
                     dim=-1
@@ -964,7 +963,7 @@ def load_bvh(
                 joint_qd[frame_indices, start_qd : start_qd + 3] = tu.angular_velocity(
                     joint_q[frame_indices, start : start + 4],
                     joint_q[frame_indices + 1, start : start + 4],
-                    dt
+                    frame_time
                 )
             elif type == df.JOINT_REVOLUTE:
                 joint_quat = joint_xforms[:, bvh_index, 3:]
@@ -1010,7 +1009,7 @@ def load_bvh(
                 joint_qd[frame_indices, start_qd : start_qd + 1] = (
                     joint_q[frame_indices + 1, start : start + 1]
                     - joint_q[frame_indices, start : start + 1]
-                ) / dt
+                ) / frame_time
 
     # 4. return the frame time and joint_q array.
     return frame_time, frames, joint_q, joint_q_mask, joint_qd